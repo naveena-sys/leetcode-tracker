8ce56fea061a1456e1980ed280131701577bdfad
naveena <23cs065@drngpit.ac.in>
2026-07-11 14:06:44 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.java
@@ -0,0 +1,21 @@
+// Last updated: 7/11/2026, 2:06:44 PM
+class Solution
+ {
+    public int[] plusOne(int[] digits) 
+    {
+        int n =digits.length;
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            if(digits[i]<9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int[] result = new int[n+1];
+           result[0] = 1;
+           return result;
+    }          
+ 
+}       
