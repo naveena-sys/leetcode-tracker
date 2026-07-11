318fedcf145eb5db1fb23fe175a6ea442c5b551a
naveena <23cs065@drngpit.ac.in>
2026-07-11 14:03:05 +0530

    Create 1781-CheckIfTwoStringArraysAreEquivalent

---
+++ 1781-CheckIfTwoStringArraysAreEquivalent/1781-CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,16 @@
+// Last updated: 7/11/2026, 2:03:05 PM
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String str1=" " ;
+        String str2=" ";
+        for(int i=0;i<word1.length;i++){
+            str1+=word1[i];
+        }
+        for(int i=0;i<word2.length;i++){
+            str2+=word2[i];
+        }if(str1.equals(str2)){
+            return true;
+        }
+        return false;
+    }
+}
