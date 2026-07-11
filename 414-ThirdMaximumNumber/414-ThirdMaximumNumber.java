36a0d4bc724d06c0554f88807c99280d4ef9bd8f
naveena <23cs065@drngpit.ac.in>
2026-07-11 14:04:17 +0530

    Create 414-ThirdMaximumNumber

---
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
@@ -0,0 +1,24 @@
+// Last updated: 7/11/2026, 2:04:17 PM
+class Solution {
+    public int thirdMax(int[] nums) {
+        Integer first = null, second = null, third = null;
+        for (int num : nums) {
+            if ((first != null && num == first) || 
+                (second != null && num == second) || 
+                (third != null && num == third)) {
+                continue;
+            }
+            if (first == null || num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (second == null || num > second) {
+                third = second;
+                second = num;
+            } else if (third == null || num > third) {
+                third = num;
+            }
+        }
+        return (third == null) ? first : third;
+    }
+}
