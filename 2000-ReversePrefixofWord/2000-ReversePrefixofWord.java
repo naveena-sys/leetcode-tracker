2c33ce74635a14eec63a5fa3fc50735d41a6f204
naveena <23cs065@drngpit.ac.in>
2026-08-07 13:48:39 +0530

    Create 2000-ReversePrefixofWord

---
+++ 2000-ReversePrefixofWord/2000-ReversePrefixofWord.java
@@ -0,0 +1,16 @@
+// Last updated: 8/7/2026, 1:48:39 PM
+1class Solution {
+2    public String reversePrefix(String word, char ch) {
+3        int r=word.indexOf(ch);
+4        int l=0;
+5        char c[]=word.toCharArray();
+6        while(l<r){
+7            char temp=c[l];
+8            c[l]=c[r];
+9            c[r]=temp;
+10            l++;
+11            r--;
+12        }
+13        return new String(c);
+14    }
+15}
